03880a060f41f5679dee316b027b016b447f9a82
yamassx <58455442+yamassx@users.noreply.github.com>
2020-05-12 14:54:02 +0900

    メソッド抽象化

--- homework3/MenuSet.java
+++ homework3/MenuSet.java
@@ -1,5 +1,42 @@
 package poli_lecture;
 
-public class MenuSet {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MenuSet {
+	private List<Menu> menus;
+	
+	MenuSet(List<Menu> menus){
+		this.menus = menus;
+	}
+	
+	List<Menu> getMenusByType(String type){
+		List<Menu> anyTypeMenu = new ArrayList<Menu>();
+		for (Menu menu : this.menus) {
+			if(menu.getType().equals(type)) {
+				anyTypeMenu.add(menu);
+			}
+		}
+		return anyTypeMenu;
+	}
+	
+	List<Menu> getMenusByOverCalorie(int cal){
+		List<Menu> overCalMenu = new ArrayList<Menu>();
+		for (Menu menu : this.menus) {
+			if(menu.getCal() > cal) {
+				overCalMenu.add(menu);
+			}
+		}
+		return overCalMenu;
+	}
+	
+	List<Menu> getMenusBySpec(MenuSpec spec){
+		List<Menu> anySpecMenu = new ArrayList<Menu>();
+		for(Menu menu : this.menus) {	
+			if (spec.satisfiedBy(menu) == true) {					
+				anySpecMenu.add(menu);	
+			}
+		}
+		return anySpecMenu;
+	}
 }
